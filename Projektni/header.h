4d690698794f2f60ac6717c73da33af9745d0a0e
crocker-hub <75636599+crocker-hub@users.noreply.github.com>
2024-06-18 10:59:19 +0200

    Update header.h

--- Projektni/header.h
+++ Projektni/header.h
@@ -7,7 +7,7 @@ typedef struct clan {
 	char ime[30];
 	int dob;       //u godinama
 	int clanarina; //u mjesecima
-};
+}Clan;
 
 void dodajClana(int brojClanova, int maksClanova, struct clan* clanovi, FILE* file);
 void obrisiClana(int brojClanova, int redniBroj, FILE* file, struct clan* clanovi);
@@ -15,11 +15,12 @@ void sortirajClanove(struct clan* clanovi, int brojClanova);
 void produziClanarinu(int redniBroj, int brojClanova, struct clan* clanovi, int produzivanje, FILE* file, int maksClanova);
 void pokaziClanove(int brojClanova, struct clan* clanovi);
 void prikazClanarina(FILE* file, struct clan* clanovi, int maksClanova, int brojClanova);
+int usporedi(const void* a, const void* b);
 
-#ifndef GLOBALS_H
-#define GLOBALS_H
-
-extern int brojClanova;
-extern int maksClanova;
-
-#endif
+//#ifndef GLOBALS_H
+//#define GLOBALS_H
+//
+//extern int brojClanova;
+//extern int maksClanova;
+//
+//#endif
